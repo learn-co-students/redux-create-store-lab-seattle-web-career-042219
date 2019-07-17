b54472eb1acbf1be8fca6f71089a3403eb951e28
github username <gregm9999@gmail.com>
2019-07-16 22:11:23 -0700

    Done.

--- src/reducers/candyReducer.js
+++ src/reducers/candyReducer.js
@@ -1,6 +1,6 @@
 function candyReducer(state = [], action) {
   switch (action.type) {
-    case 'ADD_CANDY':
+    case "ADD_CANDY":
       return [...state, action.candy];
     default:
       return state;
